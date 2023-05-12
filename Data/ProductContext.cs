98395df802838fe60b9014c59f49a3e4e3d5520e
Ryan Peelman <ryan.peelman@gmail.com>
2023-05-12 14:59:16 -0500

    seed test data from json files

--- Data/ProductContext.cs
+++ Data/ProductContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 public class ProductContext : DbContext
 {
@@ -7,10 +8,25 @@ public class ProductContext : DbContext
 
     public ProductContext(DbContextOptions<ProductContext> options) : base(options)
     {
+        //Note: for debugging purposes, this should be removed
+        SeedData();
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseInMemoryDatabase("Product");
+    }
+
+    private void SeedData()
+    {
+        var clayData = File.ReadAllText("Data/clays.json");
+        var clays = JsonConvert.DeserializeObject<List<ClayProduct>>(clayData) ?? new List<ClayProduct>();
+        Clays?.AddRange(clays);
+
+        var glazeData = File.ReadAllText("Data/glazes.json");
+        var glazes = JsonConvert.DeserializeObject<List<GlazeProduct>>(glazeData) ?? new List<GlazeProduct>();
+        Glazes?.AddRange(glazes);
+
+        SaveChanges();
     }
 }
